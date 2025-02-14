c1df957f2faa9e094065d5c2dd3c9d488a49f954
Sarmad Gulzar <sarmadgulzar@icloud.com>
2025-02-15 01:10:52 +0500

    Added simple pubsub package

---
+++ src/my_py_pubsub/my_py_pubsub/talker.py
@@ -0,0 +1,31 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class MinimalPublisher(Node):
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, "chatter", 10)
+
+        timer_period = 1.0
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = "Hello, World!"
+        self.publisher_.publish(msg)
+        self.get_logger().info(f"Publishing: '{msg.data}'")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MinimalPublisher()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
